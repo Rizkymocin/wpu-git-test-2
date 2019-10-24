a60743f4b9f11ead867b5533f2f20364502fa823
Rizkymocin <rizky.mohi30@gmail.com>
2019-10-24 12:18:27 +0800

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Coba Remote</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Possimus, neque! Eum ratione dolorum accusamus tempora soluta fugiat sit possimus alias sint illum provident, deserunt vero commodi assumenda cupiditate quaerat aliquid.</p>
+
+    <ul>
+        <li>sadsddads</li>
+    </ul>
 </body>
 </html>
